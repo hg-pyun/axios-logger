c593f45f1c848ea73bfd9eb65b9ffe5c3803b013
hg-pyun <phg2491@gmail.com>
2019-06-28 17:15:01 +0900

    Implement request spec

---
+++ src/logger/__test__/request.spec.js
@@ -0,0 +1,61 @@
+import { requestLogger, setGlobalConfig } from '../../index';
+import { printLog } from '../../common/print';
+
+jest.mock('axios');
+jest.mock('../../common/print');
+
+const axiosRequestConfig = {
+    data: 'dummy',
+};
+
+beforeEach(() => {
+    printLog.mockClear();
+});
+
+test('request should be return immutable AxiosRequestConfig', () => {
+    const mockLogger = jest.fn(requestLogger);
+    mockLogger(axiosRequestConfig);
+    expect(mockLogger).toReturnWith(axiosRequestConfig);
+});
+
+test('if config is undefined, logger make default log', () => {
+    requestLogger(axiosRequestConfig);
+    expect(printLog).toHaveBeenCalled();
+    expect(printLog).toBeCalledWith(expect.stringContaining('[Axios Logger]'));
+});
+
+test('if global config is defined only, logger make log with options', () => {
+    const globalConfig = {
+        prefixText: '[global custom prefix]',
+    };
+
+    setGlobalConfig(globalConfig);
+    requestLogger(axiosRequestConfig);
+    expect(printLog).toHaveBeenCalled();
+    expect(printLog).toBeCalledWith(expect.stringContaining('[global custom prefix]'));
+});
+
+test('if local config is defined only, logger make log with options', () => {
+    const localConfig = {
+        prefixText: '[local custom prefix]',
+    };
+
+    requestLogger(axiosRequestConfig, localConfig);
+    expect(printLog).toHaveBeenCalled();
+    expect(printLog).toBeCalledWith(expect.stringContaining('[local custom prefix]'));
+});
+
+test('if both global and local config are defined, local config should override global config', () => {
+    const globalConfig = {
+        prefixText: '[global custom prefix]',
+    };
+
+    const localConfig = {
+        prefixText: '[local custom prefix]',
+    };
+
+    setGlobalConfig(globalConfig);
+    requestLogger(axiosRequestConfig, localConfig);
+    expect(printLog).toHaveBeenCalled();
+    expect(printLog).toBeCalledWith(expect.stringContaining('[local custom prefix]'));
+});
--- src/logger/request.ts
+++ src/logger/request.ts
@@ -1,7 +1,20 @@
 import { AxiosRequestConfig } from 'axios';
 import { RequestLogConfig } from '../common/types';
+import { mergeWithGlobalConfig } from '../common/config';
+import StringBuilder from '../common/string-builder';
+import { printLog } from '../common/print';
 
-function requestLogger(request: AxiosRequestConfig, config: RequestLogConfig) {
+function requestLogger(request: AxiosRequestConfig, config?: RequestLogConfig) {
+
+    const buildConfig = config ? config : mergeWithGlobalConfig(config);
+
+    const stringBuilder = new StringBuilder(buildConfig);
+    const log = stringBuilder
+        .makePrefix()
+        .makeDateFormat()
+        .build();
+
+    printLog(log);
 
     return request;
 }
